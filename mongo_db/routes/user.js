a5002d12ed0b389e10a931d995028e2c14cbbde8
NIKHIL SAINI <nilhil484@gmail.com>
2026-03-14 00:17:35 +0530

    added user route

--- mongo_db/routes/user.js
+++ mongo_db/routes/user.js
@@ -2,25 +2,26 @@ import express from "express";
 import { userMiddleware } from "../middleware/user.js";
 import Router from "express";
 import { User, Course } from "../database/mongo.js";
+import { log } from "node:console";
 
 const userRouter = Router()
 userRouter.use(express.json())
 
 userRouter.post("/signup", async (req, res) => {
     const { username, password } = req.body;
-    if(!username || !password){
+    if (!username || !password) {
         return res.status(400).json({
             msg: "username and password required."
         })
     }
-    const exist = await User.findOne({username})
+    const exist = await User.findOne({ username })
     if (exist) {
         return res.json({
             msg: "User already signed up with these credentials."
         })
-    } 
+    }
     await User.create(
-        {username: username, password: password}
+        { username: username, password: password }
     )
     return res.status(201).json({
         msg: "User succesfully signed up"
@@ -30,26 +31,29 @@ userRouter.post("/signup", async (req, res) => {
 
 userRouter.post("/courses/:courseID", userMiddleware, async (req, res) => {
     // purchasing a course by a user
-    const courseID = req.params.courseID;
-    const username = req.headers.username;
-    User.updateOne(
-        { username: username },
-        { "$push": { purchasedCourses: courseID } }
-    )
-    return res.json({
-        msg: "purchased successfully",
-        id: courseID
-    })
-})
-
-userRouter.get("/courses", async (req, res) => {
-    // get the purchased courses by the user
     try {
-        const courses = await Course.find({})
-        return res.status(200).json({ courses });
-    } catch (error) {
-        return res.status(500).json({ msg: "something went wrong!" })
+        const courseID = req.params.courseID;
+        const username = req.headers.username;
+        await User.updateOne(
+            { username: username },
+            { "$push": { purchasedCourses: courseID } }
+        )
+        return res.json({
+            msg: "purchased successfully",
+            id: courseID
+        })
+    } catch (e) {
+        console.log(e);
     }
 })
 
-export {userRouter};
+userRouter.get("/purchasedCourses", userMiddleware, async (req, res) => {
+    // get the purchased courses by the user
+   const username = req.headers.username;
+   const user = await User.findOne({username}).populate("purchasedCourses", "title");
+   return res.json({
+    your_courses: user.purchasedCourses,
+});
+});
+
+export { userRouter };
